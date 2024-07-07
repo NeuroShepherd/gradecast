b79359c42882dad502440972076028596dc51ec7
Patrick Callahan <patricktcallahan18@gmail.com>
2024-07-07 17:29:52 +0200

    Begin writing tests

---
+++ tests/testthat/test-calculate_grade_range.R
@@ -0,0 +1,22 @@
+test_that("calculate grade range works", {
+
+  # Test the function with the default top and worst grades
+  expect_equal(calculate_grade_range(120, 3.5, 60), c(best_outcome = 8/3, worst_outcome = 12/3))
+
+  # Test the function with custom top and worst grades
+  expect_equal(calculate_grade_range(120, 3.5, 60, 1, 5), c(best_outcome = 8/3, worst_outcome = 12/3))
+
+  # Test the function with a negative number of completed ECTS
+  expect_error(calculate_grade_range(-1, 3.5, 60), "completed_ects must be >= 0")
+
+  # Test the function with a negative number of remaining ECTS
+  expect_error(calculate_grade_range(120, 3.5, -1), "remaining_ects must be >= 0")
+
+  # Test the function with a current grade below the top grade
+  expect_error(calculate_grade_range(120, 3.5, 60, 4, 5), "current_grade must be between top_grade and worst_grade")
+
+  # Test the function with a current grade above the worst grade
+  expect_error(calculate_grade_range(120, 3.5, 60, 1, 3), "current_grade must be between top_grade and worst_grade")
+})
+
+
---
+++ tests/testthat/test-convert_to_freedom_units.R
@@ -0,0 +1,23 @@
+test_that("conversion from German to US works", {
+
+  expect_vector(convert_to_freedom_units(1:5))
+  expect_length(convert_to_freedom_units(1:5), 5)
+
+
+  expect_equal(convert_to_freedom_units(1), 4)
+  expect_equal(convert_to_freedom_units(2), 3)
+  expect_equal(convert_to_freedom_units(3), 2)
+  expect_equal(convert_to_freedom_units(4), 1)
+  expect_equal(convert_to_freedom_units(5), 0)
+  expect_equal(convert_to_freedom_units(1:5), 4:0)
+
+})
+
+
+test_that("conversion from German to custom US functions", {
+
+  expect_vector(convert_to_freedom_units(1:5, max_passing_grade = 4, min_passing_grade = 2))
+  expect_length(convert_to_freedom_units(1:5, max_passing_grade = 4, min_passing_grade = 2), 5)
+
+
+})
