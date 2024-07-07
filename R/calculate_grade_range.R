9e8989d488d195f86592ab00661f713270f98170
Patrick Callahan <patricktcallahan18@gmail.com>
2024-07-07 17:29:46 +0200

    Begin creating error conditions

--- R/calculate_grade_range.R
+++ R/calculate_grade_range.R
@@ -20,6 +20,14 @@
 #'
 calculate_grade_range <- function(completed_ects, current_grade, remaining_ects,
                                   top_grade = 1, worst_grade = 5) {
+
+  grade_min_max <- c(min = min(top_grade, worst_grade), max = max(top_grade, worst_grade))
+  assertthat::assert_that(completed_ects >= 0, msg = "completed_ects must be >= 0")
+  assertthat::assert_that(remaining_ects >= 0, msg = "remaining_ects must be >= 0")
+  assertthat::assert_that(current_grade >= grade_min_max['min'],
+                          current_grade <= grade_min_max['max'],
+                          msg = "current_grade must be between top_grade and worst_grade")
+
   # Calculate the weighted min and max grade
   worst_outcome <- (completed_ects * current_grade + remaining_ects * worst_grade) / (completed_ects + remaining_ects)
   best_outcome <- (completed_ects * current_grade + remaining_ects * top_grade) / (completed_ects + remaining_ects)
