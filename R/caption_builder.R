f321015b7d0b7932387f53fcb4438001e3b80a6d
Patrick Callahan <patricktcallahan18@gmail.com>
2024-07-08 11:56:54 +0200

    Redocument project

--- R/caption_builder.R
+++ R/caption_builder.R
@@ -1,5 +1,21 @@
 
 
+#' Caption builder
+#'
+#' Builds the text caption for the grade range plot. The caption can be optionally turned off on the ggplot2 object which is useful when the text is not needed or rendered well on the image file. Instead, the Shiny app creates a separate text box for the caption.
+#'
+#' @param min_passing_grade the worst possible grade
+#' @param max_passing_grade the best possible grade
+#' @param caption_width text width for the caption. default 150
+#' @param show_caption whether or not to display the text caption
+#'
+#' @return a character string
+#' @export
+#'
+#' @examples
+#'
+#' caption_builder(min_passing_grade = 5, max_passing_grade = 1)
+#'
 caption_builder <- function(min_passing_grade, max_passing_grade, caption_width = 150, show_caption = TRUE) {
 
   cap <- glue::glue("The shaded region represents the range of all possible grade averages that can be achieved based on three factors: 1) the current grade average or GPA, 2) the number of ECTS credits already completed, 3) and the amount of remaining ECTS to complete.\n\nThe red line indicates the worst possible scenario i.e. obtaining {min_passing_grade}s in all classes while the green line indicates the best possible scenario i.e. obtaining {max_passing_grade}s in all classes. The blue dashed line represents the highest passing grade, a 1.0 and a 4.0 on the common German and American scales, respectively.") %>%
